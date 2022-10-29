8d69eb620ad5c65f15fdb50d162141be46c18eb1
Lara <lara1907714@miuegypt.edu.eg>
2022-10-30 01:25:56 +0200

    Homepage update

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,28 @@ class MyApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: Home(),
+        bottomNavigationBar: new Theme(
+          data: Theme.of(context).copyWith(
+              canvasColor: Color(0xff0D0B26),
+              primaryColor: Color(0xff0D0B26),
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent),
+          child: new BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Color.fromARGB(255, 255, 255, 255),
+            unselectedItemColor: Color(0xffA8A8A8),
+            items: [
+              new BottomNavigationBarItem(
+                  icon: Icon(Icons.home_rounded), label: "Home"),
+              new BottomNavigationBarItem(
+                  icon: Icon(Icons.library_books_outlined), label: "Library"),
+              new BottomNavigationBarItem(
+                  icon: Icon(Icons.person_outline), label: "Profile")
+            ],
+            // currentIndex: 0,
+            // onTap: (){},
+          ),
+        ),
       ),
     );
   }
