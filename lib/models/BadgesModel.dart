c036417ea5a0cc765da4b9cd29ecbe2271b6eec0
salma ahmed <96626592+samlaahmed@users.noreply.github.com>
2022-12-26 17:02:42 +0200

    Update BadgesModel.dart

--- lib/models/BadgesModel.dart
+++ lib/models/BadgesModel.dart
@@ -2,5 +2,41 @@ import "package:cardflip/data/dummy_data.dart";
 import "package:cardflip/data/Badges.dart";
 
 class BadgesModel {
-  final List<Badges> badges = [];
+  double? score;
+  int? time;
+  final List<Badges> badges = [
+    Badges(
+        name: "cardfliper",
+        //image:fromdatabasewhenitisdone
+        description: "for completing your first quiz on cardFlip"),
+    Badges(
+        name: "champion",
+        //image:fromdatabasewhenitisdone
+        description: "for scoring 100 points"),
+    Badges(
+        name: "striver",
+        //image:fromdatabasewhenitisdone
+        description: "for scoring 100 points in less than a minute"),
+    Badges(
+        name: "genius",
+        //image:fromdatabasewhenitisdone
+        description: "for scoring 200 points in less than a minute"),
+    Badges(
+        name: "the og cardfliper",
+        //image:fromdatabasewhenitisdone
+        description: "for scoring 1000 points")
+  ];
+  conditions(name) {
+    if (time == 60) {
+      return badges[0];
+    } else if (score == 100) {
+      return badges[1];
+    } else if (score == 100 && time! < 60) {
+      return badges[2];
+    } else if (score == 200 && time! < 60) {
+      return badges[3];
+    } else if (score == 1000) {
+      return badges[4];
+    }
+  }
 }
