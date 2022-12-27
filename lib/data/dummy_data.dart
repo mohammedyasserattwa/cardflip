cf22d0bf3e056f06f51b3cb91156f41a286267a1
Shehab ElDin Mohsen Hamed <shehabeldin1911202@miuegypt.edu.eg>
2022-12-27 20:00:39 +0200

    Admin Users Page added

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -4,15 +4,24 @@ class DummyData {
   String FirstName = "Lara";
   String LastName = "Ghoniem";
   String username = "laraghoniem";
+  String FirstName2="Shehab ElDin";
+  String LastName2 = "Mohsen";
+  String username2 = "shehabemohsen";
   int flashcard = 49;
   double ratingnumber = 4.0;
 
   String get fname {
     return FirstName;
   }
+  String get fname2 {
+    return FirstName2;
+  }
 
   String get lname {
     return LastName;
+  }
+  String get lname2 {
+    return LastName2;
   }
 
   int get flashcardnumber {
