74c89fc2d9d6aa8d67b8e54f02b1f575f05e436b
Lara <lara1907714@miuegypt.edu.eg>
2022-12-30 01:41:17 +0200

    test page update

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -3,6 +3,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:no_glow_scroll/no_glow_scroll.dart';
 import "package:flutter/material.dart";
 import '../models/testModel.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../data/Test.dart';
 import '../data/card_generator.dart';
 
@@ -35,28 +36,119 @@ class _TestState extends State<Test> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 25),
-                  child: Container(
-                    child: Text(""),
-                    width: 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("Images/icons/close_button.png")),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 40),
+                      child: Container(
+                        child: Text(""),
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image:
+                                  AssetImage("Images/icons/close_button.png")),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+                SizedBox(
+                  width: 205,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(left: 20.0, right: 20.0, top: 40.0),
+                  child: SizedBox(
+                    width: 55,
+                    height: 55,
+                    child: PopupMenuButton(
+                      color: Colors.white,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8.0),
+                        ),
+                      ),
+                      icon: SvgPicture.asset("Images/icons/svg/more-fill.svg"),
+                      itemBuilder: (BuildContext context) => [
+                        PopupMenuItem(
+                          child: Text(""),
+                          // child: GestureDetector(
+                          //   onTap: () {
+                          //     Navigator.pushNamed(context, "/settings");
+                          //   },
+                          //   child: Row(
+                          //     children: [
+                          //       SvgPicture.asset(
+                          //           "Images/icons/svg/gear-fill.svg"),
+                          //       const SizedBox(
+                          //         width: 10,
+                          //       ),
+                          //       const Text("Settings"),
+                          //     ],
+                          //   ),
+                          // ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
             SizedBox(
-              height: 175,
+              height: 25,
+            ),
+            Center(
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+                        width: 350,
+                        height: 512,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("Images/cards/testpage/1.png")),
+                        ),
+                        child: const Text(""),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 350,
+                    height: 512,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("Images/cards/testpage/2.png")),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 100,
+                      ),
+                      Container(
+                        width: 350,
+                        height: 512,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("Images/cards/testpage/0.png")),
+                        ),
+                        child: const Text(""),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ]),
     ));
