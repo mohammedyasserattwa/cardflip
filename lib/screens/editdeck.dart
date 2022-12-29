e8010d013b9d238ddcef5bf002e419326849e2d1
salma ahmed <96626592+samlaahmed@users.noreply.github.com>
2022-12-29 21:47:33 +0200

    update edit and add deck (both not final)

--- lib/screens/editdeck.dart
+++ lib/screens/editdeck.dart
@@ -6,3 +6,115 @@ import '../data/card_generator.dart';
 // import '../models/libraryModel.dart';
 import '../widgets/navibar.dart';
 import '../widgets/deck.dart';
+
+// main() => runApp(MaterialApp(
+//       home: Editdeck(),
+//     ));
+
+class Editdeck extends StatefulWidget {
+  const Editdeck({key});
+
+  @override
+  State<Editdeck> createState() => _EditdeckState();
+}
+
+class _EditdeckState extends State<Editdeck> {
+  @override
+  Widget build(BuildContext context) {
+    final height = 128.67;
+    final width = 133.67;
+    return Scaffold(
+      body: Container(
+        height: 1000,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("Images/backgrounds/deckbackground.png"),
+              fit: BoxFit.cover),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "Images/icons/arrow-left-s-line.png"),
+                            fit: BoxFit.cover),
+                      ),
+                      width: 40,
+                      height: 40,
+                      child: const Text(""))),
+            ),
+            Text(
+              "Edit Deck",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "PolySans_Median",
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+                fontSize: 48,
+              ),
+            ),
+            Expanded(
+              child: ListView(children: [
+                Text(
+                  "Title",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontFamily: "PolySans_Median",
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                    fontSize: 24,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(40),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none, //<-- SEE HERE
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.black.withOpacity(0.1),
+                    hintText: 'Enter the deck title',
+                  ),
+                ),
+                Text(
+                  "Description",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontFamily: "PolySans_Median",
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                    fontSize: 24,
+                  ),
+                ),
+                TextField(
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      //<-- SEE HERE
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.black.withOpacity(0.1),
+                    hintText: 'Enter the deck description',
+                  ),
+                ),
+              ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
