a30aeb2a59c4bee69d1ecd2046a9b41d2132ef33
salma ahmed <96626592+samlaahmed@users.noreply.github.com>
2023-01-04 20:20:20 +0200

    add deck update

--- lib/screens/adddeck.dart
+++ lib/screens/adddeck.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: unnecessary_new
 
+//import 'dart:html';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:no_glow_scroll/no_glow_scroll.dart';
+import 'package:flutter_multi_select_items/flutter_multi_select_items.dart';
 import "package:flutter/material.dart";
 import '../data/dummy_data.dart';
 import '../data/card_generator.dart';
@@ -23,19 +26,21 @@ class Adddeck extends StatefulWidget {
 class _AdddeckState extends State<Adddeck> {
   @override
   Widget build(BuildContext context) {
-    final height = 128.67;
-    final width = 133.67;
-    return Scaffold(
+    Size size = MediaQuery.of(context).size;
+
+    return SafeArea(
+        child: Scaffold(
       body: Container(
-        height: 1000,
-        width: double.infinity,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
         decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("Images/backgrounds/deckbackground.png"),
-              fit: BoxFit.cover),
+            image: AssetImage("Images/backgrounds/deckbackground.png"),
+            fit: BoxFit.cover,
+          ),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
@@ -112,11 +117,78 @@ class _AdddeckState extends State<Adddeck> {
                     hintText: 'Enter the deck description',
                   ),
                 ),
+                Text(
+                  "Tags",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontFamily: "PolySans_Median",
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                    fontSize: 24,
+                  ),
+                ),
+                MultiSelectContainer(
+                    itemsDecoration: MultiSelectDecorations(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                            Colors.black.withOpacity(0.1),
+                            Colors.black.withOpacity(0.1),
+                          ]),
+                          border:
+                              Border.all(color: Colors.black.withOpacity(0.1)),
+                          borderRadius: BorderRadius.circular(20)),
+                      selectedDecoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                              colors: [Colors.black, Colors.grey]),
+                          border: Border.all(color: Colors.blueGrey[700]!),
+                          borderRadius: BorderRadius.circular(5)),
+                      disabledDecoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.1),
+                          border:
+                              Border.all(color: Colors.black.withOpacity(0.1)),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    items: [
+                      MultiSelectCard(value: 'Dart', label: 'Dart'),
+                      MultiSelectCard(value: 'Python', label: 'Python'),
+                      MultiSelectCard(
+                        value: 'JavaScript',
+                        label: 'JavaScript',
+                      ),
+                      MultiSelectCard(value: 'Java', label: 'Java'),
+                      MultiSelectCard(value: 'C#', label: 'C#'),
+                      MultiSelectCard(value: 'C++', label: 'C++'),
+                    ],
+                    onChange: (allSelectedItems, selectedItem) {}),
               ]),
+            ),
+            Container(
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 20, right: 20),
+                  child: SizedBox(
+                    height: 40,
+                    width: 100,
+                    child: ElevatedButton(
+                      onPressed: () => "done",
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black.withOpacity(0.1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                      child: Text(
+                        "Done",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-    );
+    ));
   }
 }
