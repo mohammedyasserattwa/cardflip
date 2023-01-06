5f4ebfe7497a8b07fef596d9bfa833106980ac92
Omar Wael <78542693+OmarElaskalaany@users.noreply.github.com>
2023-01-06 22:45:13 +0200

    Update add flashcards page

--- lib/screens/add_flashcards.dart
+++ lib/screens/add_flashcards.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import "package:flutter/material.dart";
+import 'package:no_glow_scroll/no_glow_scroll.dart';
 
 class AddFlashcards extends StatefulWidget {
   const AddFlashcards({super.key});
@@ -10,10 +11,8 @@ class AddFlashcards extends StatefulWidget {
 }
 
 class _AddFlashcardsState extends State<AddFlashcards> {
-  final _keyParams = {
-    "Term": GlobalKey<FormState>(),
-    "Defination": GlobalKey<FormState>(),
-  };
+  List ControllerDefinitionData = [TextEditingController()];
+  List ControllerTermData = [TextEditingController()];
 
   List resultedData = [Column()];
   @override
@@ -29,50 +28,54 @@ class _AddFlashcardsState extends State<AddFlashcards> {
             ),
             child: Column(
               children: [
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(top: 50),
-                      alignment: Alignment.centerLeft,
-                      child: GestureDetector(
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
-                          child: Container(
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                    image: AssetImage(
-                                        "Images/icons/arrow-left-s-line.png"),
-                                    fit: BoxFit.cover),
-                              ),
-                              width: 40,
-                              height: 40,
-                              child: const Text(""))),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 10),
-                      child: Text('Add Flashcard',
-                          style: TextStyle(
-                              color: Color(0xFF191C32),
-                              fontFamily: 'Poppins',
-                              fontSize: 45,
-                              fontWeight: FontWeight.w500)),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15, right: 15),
+                Container(
+                  padding: EdgeInsets.only(top: 50),
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
                       child: Container(
-                        height: 360,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: Colors.grey.withOpacity(0.25)),
-                        child: Expanded(
-                          child: ListView.builder(
-                              itemCount: resultedData.length,
-                              itemBuilder: ((context, index) {
-                                return Column(
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    "Images/icons/arrow-left-s-line.png"),
+                                fit: BoxFit.cover),
+                          ),
+                          width: 40,
+                          height: 40,
+                          child: const Text(""))),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text('Add Flashcard',
+                      style: TextStyle(
+                          color: Color(0xFF191C32),
+                          fontFamily: 'Poppins',
+                          fontSize: 45,
+                          fontWeight: FontWeight.w500)),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Expanded(
+                    child: NoGlowScroll(
+                  child: ListView(
+                    children: [
+                      Column(
+                        children: [
+                          for (int index = 0;
+                              index < resultedData.length;
+                              index++)
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15, right: 15, bottom: 25),
+                              child: Container(
+                                height: 360,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    color: Colors.grey.withOpacity(0.25)),
+                                child: Column(
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: [
@@ -99,21 +102,26 @@ class _AddFlashcardsState extends State<AddFlashcards> {
                                                       resultedData[index]);
                                                 });
                                               },
-                                              child: Container(
-                                                height: 40,
-                                                width: 40,
-                                                padding: const EdgeInsets.only(
-                                                    left: 15, top: 20),
-                                                decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(16),
-                                                  image: DecorationImage(
-                                                    image: AssetImage(
-                                                        "Images/icons/trash.png"),
-                                                    fit: BoxFit.cover,
-                                                  ),
-                                                ),
-                                              ),
+                                              child: index == 0
+                                                  ? Container()
+                                                  : Container(
+                                                      height: 40,
+                                                      width: 40,
+                                                      padding:
+                                                          const EdgeInsets.only(
+                                                              left: 15,
+                                                              top: 20),
+                                                      decoration: BoxDecoration(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(16),
+                                                        image: DecorationImage(
+                                                          image: AssetImage(
+                                                              "Images/icons/trash.png"),
+                                                          fit: BoxFit.cover,
+                                                        ),
+                                                      ),
+                                                    ),
                                             ),
                                           ),
                                         ],
@@ -122,6 +130,7 @@ class _AddFlashcardsState extends State<AddFlashcards> {
                                         padding: const EdgeInsets.only(
                                             top: 10, left: 12.0, right: 12),
                                         child: TextFormField(
+                                          controller: ControllerTermData[index],
                                           key: GlobalKey<FormState>(),
                                           decoration: InputDecoration(
                                             filled: true,
@@ -165,8 +174,9 @@ class _AddFlashcardsState extends State<AddFlashcards> {
                                                 BorderRadius.circular(16),
                                           ),
                                           height: 130,
-                                          child: TextFormField(
-                                            maxLines: null,
+                                          child: TextField(
+                                            controller:
+                                                ControllerDefinitionData[index],
                                             key: GlobalKey<FormState>(),
                                             decoration: InputDecoration(
                                               border: OutlineInputBorder(
@@ -194,60 +204,58 @@ class _AddFlashcardsState extends State<AddFlashcards> {
                                           ),
                                         ),
                                       ),
-                                      SizedBox(
-                                        height: 30,
-                                      )
-                                    ]);
-                              })),
-                        ),
+                                    ]),
+                              ),
+                            ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  padding: EdgeInsets.only(right: 20),
-                  alignment: Alignment.centerRight,
-                  child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          resultedData.add(Column());
-                        });
-                      },
-                      child: Container(
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage("Images/icons/add.png"),
-                                fit: BoxFit.cover),
-                          ),
-                          width: 40,
-                          height: 40,
-                          child: const Text(""))),
-                ),
-                SizedBox(
-                  height: 150,
-                ),
-                Container(
-                  padding: EdgeInsets.only(right: 20),
-                  alignment: Alignment.centerRight,
-                  child: GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                              color: Colors.grey.withOpacity(0.30)),
-                          width: 85,
-                          height: 35,
-                          child: const Text("Done",
-                              style: TextStyle(
-                                  color: Color(0xFF191C32),
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500)))),
-                ),
+                      Container(
+                        padding: EdgeInsets.only(right: 20),
+                        alignment: Alignment.centerRight,
+                        child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                resultedData.add(Column());
+                                ControllerDefinitionData.add(
+                                    TextEditingController());
+                                ControllerTermData.add(TextEditingController());
+                              });
+                            },
+                            child: Container(
+                                decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage("Images/icons/add.png"),
+                                      fit: BoxFit.cover),
+                                ),
+                                width: 40,
+                                height: 40,
+                                child: const Text(""))),
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(right: 20),
+                        alignment: Alignment.centerRight,
+                        child: GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    color: Colors.grey.withOpacity(0.30)),
+                                width: 85,
+                                height: 35,
+                                child: const Text("Done",
+                                    style: TextStyle(
+                                        color: Color(0xFF191C32),
+                                        fontFamily: 'Poppins',
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500)))),
+                      ),
+                    ],
+                  ),
+                )),
               ],
             )));
   }
